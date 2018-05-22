29c23286d03707ceb2737738e79031fdc47bc2a6
Eddy Leon <xltunited@gmail.com>
2018-05-22 17:00:49 -0400

    'Meet Catalina' page complete

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,22 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    // Active Menu Item
+    console.log($('.navigation li a').attr('href'));
+    console.log(window.location.pathname);
+
+    $('.navigation li a').each( function(){
+
+      var url = document.createElement('a');
+
+      url.href = $(this).attr('href');
+
+      if (url.href == window.location.href) {
+        $(this).addClass("menu-active");
+      }
+
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
---
+++ resources/views/template-meet.blade.php
@@ -0,0 +1,30 @@
+{{--
+  Template Name: Meet Catalina Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+
+  <div class="section-container">
+
+    <div class="text-section box-green">
+      <h1 class="text-top">MEET CATALINA</h1>
+      <h1 class="text-title meet-title">CATALINA CRUZ</h1>
+      <h1 class="text-mid">Catalina Cruz is an experienced attorney and a leader for immigration reform and workers’ rights.</h1>
+      <h1 class="text-bot"><i class="fa fa-angle-down"></i></h1>
+    </div>
+
+    <div class="image-section">
+      <img src="@asset('images/CC_-249.jpg')"/>
+    </div>
+  
+  </div>
+
+  <div class="text-body">
+    <?php the_content(); ?>
+  </div>
+
+  @endwhile
+@endsection

e12f6aa70c813e5c8c3c6f6764c043b740ebb6dd
Eddy Leon <xltunited@gmail.com>
2018-05-21 22:42:52 -0400

    first home section

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -7,5 +7,19 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
   
+  <div class="section-container">
+
+    <div class="text-section top-section box-blue">
+      <h1 class="text-top home-top">DARE TO</h1>
+      <h1 class="text-mid home-mid">DREAM</h1>
+      <h1 class="text-bot home-bot">DEMOCRAT FOR ASSEMBLY</h1>
+    </div>
+
+    <div class="image-section top-section">
+      <img src="@asset('images/C-CRUZ_5_11_181708_X1_RGB.jpg')"/>
+    </div>
+  
+  </div>
+
   @endwhile
 @endsection

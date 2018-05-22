e5806f8fe3a589944934b91a70817f74d6d1c9c1
Eddy Leon <xltunited@gmail.com>
2018-05-22 02:32:41 -0400

    homepage complete

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -33,11 +33,61 @@
 
     <div class="text-section box-green">
       <h1 class="text-top">MEET CATALINA</h1>
-      <h1 class="text-mid">Catalina Cruz is an experienced attorney and a leader for immigration reform and workers’ rights. If elected, she would be the first DREAMer elected in New York state.</h1>
-      <a class="learn-more" href="/meet-catalina"><i class="fa fa-arrow-right"></i>&nbsp;<p>Learn More</p</a>
+      <h1 class="text-mid no-learn">Catalina Cruz is an experienced attorney and a leader for immigration reform and workers’ rights. If elected, she would be the first DREAMer elected in New York state.</h1>
+      <a class="learn-more" href="/meet-catalina"><i class="fa fa-arrow-right"></i>&nbsp;<p>Learn More</p></a>
     </div>
   
   </div>
 
+  {{-- Section 3 --}}
+  
+  <div class="section-container">
+
+    <div class="text-section box-red">
+      <h1 class="text-top">ISSUES</h1>
+      <h1 class="text-title">CATALINA'S PLAN</h1>
+      <h1 class="text-mid">Catalina has a smart and innovative plan to improve our community. </h1>
+      <a class="learn-more" href="/issues"><i class="fa fa-arrow-right"></i>&nbsp;<p>Learn More</p></a>
+    </div>
+
+    <div class="image-section">
+      <img src="@asset('images/CC_-140.jpg')"/>
+    </div>
+  
+  </div>
+
+   {{-- Section 4 --}}
+  
+  <div class="section-container">
+
+    <div class="image-section">
+      <img src="@asset('images/7Y5A6946-high-res-adjusted-flat.jpg')"/>
+    </div>
+
+    <div class="text-section box-blue">
+      <h1 class="text-top">NEWS</h1>
+      <h1 class="text-title">NEWS ABOUT CATALINA</h1>
+      <h1 class="text-mid">The latest news about Catalina’s campaign and our community.</h1>
+      <a class="learn-more" href="/news"><i class="fa fa-arrow-right"></i>&nbsp;<p>Learn More</p></a>
+    </div>
+
+  </div>
+  
+  {{-- Section 5 --}}
+  
+  <div class="section-container">
+
+    <div class="text-section box-green">
+      <h1 class="text-top">OUR COMMUNITY</h1>
+      <h1 class="text-title text-title-small">CORONA<br>JACKSON HEIGHTS<br>ELMHURST</h1>
+      <h1 class="text-mid">Catalina seeks to represent these diverse communities of Queens. </h1>
+      <a class="learn-more" href="/events"><i class="fa fa-arrow-right"></i>&nbsp;<p>Learn More</p></a>
+    </div>
+
+    <div class="image-section">
+      <img src="@asset('images/CC_-294.jpg')"/>
+    </div>
+  
+  </div>
   @endwhile
 @endsection
